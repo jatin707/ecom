5cd9087d91be8ea6d3427f372f76fb28f827119e
unknown <jatin707>
2021-05-31 11:24:43 +0530

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'ppk_test_51Ix3DWSBuifiQ5AGLS8uRAmKRzatqe0rYFYpgp698TuAhUYqV7ZhSTJkfPpQR494Ke0RiVUobtjXZaQZwYc91D3P00I4ggkCve';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Keto Food Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://i.ibb.co/P6bMZZ7/logo-1.png'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      currency="INR"
+      panelLabel='Pay Now'
+      bitcoin
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
