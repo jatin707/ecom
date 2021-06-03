03f785fee60a64df198471dcdcc5dc0d94783ece
unknown <jatin707>
2021-06-03 18:25:09 +0530

    corrected service worker

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {store, persistor} from './redux/store';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
-import * as serviceworker from './serviceWorker';
+import * as serviceworker from '../src/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
